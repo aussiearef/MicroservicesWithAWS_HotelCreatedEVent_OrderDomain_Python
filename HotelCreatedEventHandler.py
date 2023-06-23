bad8bf775eddffd6de65bcf97b1baec3ef4b7278
Aref Karimi <arefkr@gmail.com>
2023-06-24 07:35:46 +1000

    Update HotelCreatedEventHandler.py

--- HotelCreatedEventHandler.py
+++ HotelCreatedEventHandler.py
@@ -6,6 +6,7 @@ from boto3 import dynamodb
 def handler(event, context):
 
     message_id = event['Records'][0]['Sns']["MessageId"]
+    
     sns_message = event['Records'][0]['Sns']["Message"]
     sns_message_json = json.loads(sns_message)
 
@@ -13,4 +14,4 @@ def handler(event, context):
 
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(hotels_order_domain_table)
-    table.put_item(Item=sns_message_json)
+    table.put_item(Item=sns_message_json.to_dict())
